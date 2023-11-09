97ef32ff26fda66ddd3791d5f47df9d4105f1d27
Fakhriyor Jalilov <faxriyorjalilov@gmail.com>
2023-11-10 04:42:32 +0500

    added new project Age calculator app

---
+++ script.js
@@ -0,0 +1,82 @@
+//INPUTS
+let dayInp = document.getElementById("day");
+let monthInp = document.getElementById("month");
+let yearInp = document.getElementById("year");
+
+//---OUTPUT
+
+let dayOtp = document.getElementById("DD");
+let monthOtp = document.getElementById("MM");
+let yearOtp = document.getElementById("YY");
+
+//--FORM ELEMENT
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit", handleSubmit);
+
+const date = new Date();
+let day = date.getDate();
+let month = 1 + date.getMonth();
+let year = date.getFullYear();
+
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function validate() {
+  const inputs = document.querySelectorAll("input");
+  let validator = true;
+  inputs.forEach((i) => {
+    const parent = i.parentElement;
+
+    if (!i.value) {
+      i.style.borderColor = "red";
+      parent.querySelector("small").innerText = "this field is required";
+      validator = false;
+    } else if (monthInp.value > 12) {
+      monthInp.style.borderColor = "red";
+      monthInp.parentElement.querySelector("small").innerText =
+        "Must be a valid month";
+      validator = false;
+    } else if (dayInp.value > 31) {
+      dayInp.style.borderColor = "red";
+      dayInp.parentElement.querySelector("small").innerText =
+        "Must be a valid day";
+      validator = false;
+    } else if (dayInp.value > 31) {
+      dayInp.style.borderColor = "red";
+      dayInp.parentElement.querySelector("small").innerText =
+        "Must be a valid day";
+      validator = false;
+    } else if (yearInp.value > 2023) {
+      yearInp.style.borderColor = "red";
+      yearInp.parentElement.querySelector("small").innerText =
+        "Must be in the past";
+      validator = false;
+    } else {
+      i.style.borderColor = "black";
+      parent.querySelector("small").innerText = "";
+      validator = true;
+    }
+  });
+  return validator;
+}
+function handleSubmit(e) {
+  e.preventDefault();
+  if (validate()) {
+    if (dayInp.value > day) {
+      day = day + months[month - 1];
+      month = month - 1;
+    }
+    if (monthInp.value > month) {
+      month = month + 12;
+      year = year - 1;
+    }
+    const d = day - dayInp.value;
+    const m = month - monthInp.value;
+    const y = year - yearInp.value;
+
+    dayOtp.innerHTML = d;
+    monthOtp.innerHTML = m;
+    yearOtp.innerHTML = y;
+  }
+}
